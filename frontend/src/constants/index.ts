d01ac4544e5a1ce22bfb98a10983ac7463bd1e09
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-08-24 21:21:13 +0100

    Create index.ts

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,9 @@
+export const menuArray = [
+  { id: 1, name: "Home" },
+  { id: 2, name: "Blockchain" },
+  { id: 3, name: "Token" },
+  { id: 4, name: "NFTs" },
+  { id: 5, name: "Resources" },
+  { id: 6, name: "Developers" },
+  { id: 7, name: "More" },
+];
