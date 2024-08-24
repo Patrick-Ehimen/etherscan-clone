ea31147e0ba830b4ea2f753ca1b88fce00faa3da
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-08-24 21:21:40 +0100

    Create index.ts

---
+++ frontend/public/index.ts
@@ -0,0 +1,7 @@
+import Logo from "./assets/logo.png";
+import dropdownicon from "./assets/dropdownIcon.svg";
+import moonPayImg from "./assets/moonpay.webp";
+import searchImg from "./assets/search.svg";
+import charts from "./assets/chart.png";
+
+export { Logo, dropdownicon, moonPayImg, searchImg, charts };
